66bd2a1390834cf45f8f24550f992f305c884b19
azimjon-95 <azimjon995m@gmail.com>
2023-03-06 07:58:19 +0500

    olma front

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const minURL = axios.create({
-  // baseURL: "https://olma-uz.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://olma-uz.onrender.com",
+  // baseURL: "http://localhost:5000",
 });
 console.log(minURL);
 
